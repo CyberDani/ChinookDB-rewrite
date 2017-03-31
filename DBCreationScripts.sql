ce9b63981a51e49cbad031a77d0c7ad4145d35ed
Balasko.Laszlo <balaskolaszlo95@gmail.com>
2017-03-31 22:52:47 +0300

    DB Creation Scripts #2

--- DBCreationScripts.sql
+++ DBCreationScripts.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Artist
 (
 Id int PRIMARY KEY,
-Name varchar(200),
+AName varchar(200),
 ); 
 
 CREATE TABLE Country
 (
 Id int PRIMARY KEY,
-Name varchar(200)
+CName varchar(200)
 );
 
 CREATE TABLE City
 (
 Id int PRIMARY KEY,
-Name varchar(200)
+CiName varchar(200)
 );
 
 CREATE TABLE Album(
@@ -25,7 +25,39 @@ ReleaseDate date
 
 CREATE TABLE Adress(
 PostalCode int PRIMARY KEY,
-Address varchar(200),
+Addr varchar(200),
 CityId int REFERENCES City(Id),
 CountryId int REFERENCES Country(Id)
 )
+
+CREATE TABLE Concert(
+Id int PRIMARY KEY,
+PostalCode int REFERENCES Adress(PostalCode),
+CDate date,
+ArtistId int REFERENCES Artist(Id)
+)
+
+CREATE TABLE MediaType(
+Id int PRIMARY KEY,
+MName varchar(200),
+)
+
+CREATE TABLE Genre(
+Id int PRIMARY KEY,
+GName varchar(200)
+)
+
+CREATE TABLE Track(
+Id int PRIMARY KEY,
+TName varchar(200),
+AlbumId int REFERENCES Album(Id),
+MediaTypeId int REFERENCES MediaType(Id),
+GenreId int REFERENCES Genre(Id),
+Composer varchar(200),
+Miliseconds int,
+Bytes int,
+UnitPrie int
+)
+
+
+
