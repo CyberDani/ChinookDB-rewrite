40f36f268f3c2f544e7c74786dd500c63e051af1
Feldman Daniel <dani.feldman11@gmail.com>
2017-04-01 18:35:46 +0300

    3 tables done

--- DBDataTransferScripts.sql
+++ DBDataTransferScripts.sql
@@ -1,8 +1,17 @@
 /*******************************************************************************
   Feltoltes adatokkal
+  -------------------
+  Ures adatbazisra alkalmazd, mert maskepp meg fogja duplazni az adatokat!
 ********************************************************************************/
 
 use BI_source
 
 INSERT INTO Artist(AName)
-SELECT DISTINCT * FROM Chinook.dbo.Artist
+SELECT DISTINCT Name AS AName FROM Chinook.dbo.Artist
+
+INSERT INTO MediaType(MName)
+SELECT DISTINCT Name AS MName FROM Chinook.dbo.MediaType
+
+INSERT INTO Genre(GName)
+SELECT DISTINCT Name AS GName FROM Chinook.dbo.Genre
+
