48b2118a0f5301813c96acb0856c87c9de8a5c10
Feldman Daniel <dani.feldman11@gmail.com>
2017-04-02 12:22:49 +0300

    added City

--- DBDataTransferScripts.sql
+++ DBDataTransferScripts.sql
@@ -9,24 +9,36 @@ use BI_source
 INSERT INTO Artist(AName)
 SELECT DISTINCT Name AS AName FROM Chinook.dbo.Artist
 
+
 INSERT INTO MediaType(MName)
 SELECT DISTINCT Name AS MName FROM Chinook.dbo.MediaType
+
 
 INSERT INTO Genre(GName)
 SELECT DISTINCT Name AS GName FROM Chinook.dbo.Genre
 
+
 CREATE PROCEDURE TransferAlbum
 AS
 BEGIN
-SELECT cart.ArtistId,biart.Id
-INTO #ArtistData 
-FROM Chinook.dbo.Artist cart, BI_source.dbo.Artist biart
-WHERE AName= cart.Name
+	SELECT cart.ArtistId,biart.Id
+	INTO #ArtistData 
+	FROM Chinook.dbo.Artist cart, BI_source.dbo.Artist biart
+	WHERE AName= cart.Name
 
-INSERT INTO BI_source.dbo.Album(Title,ArtistId,ReleaseDate)
-SELECT Title,ad.Id,DATEADD(DAY, ABS(CHECKSUM(NEWID()) % 3650), '2000-01-01') FROM Chinook.dbo.Album calb, #ArtistData ad WHERE calb.ArtistId=ad.ArtistId
+	INSERT INTO BI_source.dbo.Album(Title,ArtistId,ReleaseDate)
+	SELECT Title,ad.Id,DATEADD(DAY, ABS(CHECKSUM(NEWID()) % 3650), '2000-01-01') 
+	FROM Chinook.dbo.Album calb, #ArtistData ad 
+	WHERE calb.ArtistId=ad.ArtistId
 END
 
-DROP PROCEDURE TransferAlbum 
+--DROP PROCEDURE TransferAlbum 
 EXECUTE TransferAlbum
 
+-- Collect countries among tables
+INSERT INTO Country(CName)
+SELECT DISTINCT Country AS CName FROM Chinook.dbo.Customer
+	UNION
+SELECT DISTINCT Country AS CName FROM Chinook.dbo.Employee
+	UNION
+SELECT DISTINCT BillingCountry AS CName FROM Chinook.dbo.Invoice
