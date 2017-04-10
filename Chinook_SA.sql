6dc0819ccd9621360c0cac7cabdadf3de51b357f
Balasko.Laszlo <balaskolaszlo95@gmail.com>
2017-04-10 18:04:40 +0300

    SA changes

--- Chinook_SA.sql
+++ Chinook_SA.sql
@@ -1,15 +1,18 @@
-CREATE DATABASE Chinook_SA
+--DROP DATABASE Chinook_SA
+--CREATE DATABASE Chinook_SA
 USE Chinook_SA
 
 CREATE TABLE Country(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+CountryID nvarchar(512),
 cName nvarchar(512),
 EXECUTION_INSTANCE_GUID nvarchar(38) not null,
 LOAD_DATE datetime not null
 )
 
 CREATE TABLE City(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+CityID nvarchar(512),
 ciName nvarchar(512),
 countryId nvarchar(512),
 EXECUTION_INSTANCE_GUID nvarchar(38) not null,
@@ -17,7 +20,8 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE Adress(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+AdressID nvarchar(512),
 addr nvarchar(512),
 cityId nvarchar(512),
 postalCode nvarchar(512),
@@ -26,14 +30,16 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE Artist(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+ArtistID nvarchar(512),
 aName nvarchar(512),
 EXECUTION_INSTANCE_GUID nvarchar(38) not null,
 LOAD_DATE datetime not null
 )
 
 CREATE TABLE Album(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+AlbumID nvarchar(512),
 title nvarchar(512),
 artistId nvarchar(512),
 releaseDate nvarchar(512),
@@ -42,7 +48,8 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE Concert(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+ConcertID nvarchar(512),
 postalCode nvarchar(512),
 cDate  nvarchar(512),
 artistId nvarchar(512),
@@ -51,7 +58,8 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE Customer(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+CustomerID nvarchar(512),
 firstName nvarchar(512),
 lastName nvarchar(512),
 phone nvarchar(512),
@@ -62,14 +70,16 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE Genre(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+GenreID nvarchar(512),
 gName nvarchar(512),
 EXECUTION_INSTANCE_GUID nvarchar(38) not null,
 LOAD_DATE datetime not null
 )
 
 CREATE TABLE Invoice(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+InvoiceID nvarchar(512),
 customerId nvarchar(512),
 invoiceDate nvarchar(512),
 postalCode nvarchar(512),
@@ -79,7 +89,8 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE InvoiceLine(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+InvoiceLineID nvarchar(512),
 invoiceId nvarchar(512),
 trackId nvarchar(512),
 unitPrice nvarchar(512),
@@ -89,14 +100,16 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE MediaType(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+MTID nvarchar(512),
 mName nvarchar(512),
 EXECUTION_INSTANCE_GUID nvarchar(38) not null,
 LOAD_DATE datetime not null
 )
 
 CREATE TABLE Tickets(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+TicketsID nvarchar(512),
 concertId nvarchar(512),
 price nvarchar(512),
 invoiceId nvarchar(512),
@@ -105,7 +118,8 @@ LOAD_DATE datetime not null
 )
 
 CREATE TABLE Track(
-ID int PRIMARY KEY IDENTITY(1,1),
+ID INT PRIMARY KEY IDENTITY(1,1),
+TrackID nvarchar(512),
 tName nvarchar(512),
 albumId nvarchar(512),
 mediaTypeId nvarchar(512),
